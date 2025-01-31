d4ce511bb5913ab42dbba2ed4128a7c6733fcc6d
leo8599117 <ian.hernandez@alumnos.udg.mx>
2025-01-30 19:35:25 -0600

    new file:   Interactive-1.ipynb 	new file:   sesion_1_CRUD.py

---
+++ sesion_1_CRUD.py
@@ -0,0 +1,24 @@
+## Sesion 1
+# %%
+import pymongo
+import yaml
+from pathlib import Path
+from pymongo import MongoClient
+
+# %% Conectarnos al servidor de MongoDB
+
+# Información necesaria para conectarnos al servidor:
+# IP
+# Puerto
+# Usuario
+# Password
+# Base de datos de autenticación
+
+
+client = MongoClient()
+
+# %%
+
+
+
+
